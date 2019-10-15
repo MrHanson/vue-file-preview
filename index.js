a8fbb5ceb403f4c5f642b5847b07623355bfe8db
MrHanson <chihanghanson@gmail.com>
2019-10-15 15:13:06 +0800

    docs: v0.1.1

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-/** import element-ui */
-import ElementUI from 'element-ui'
-Vue.use(ElementUI)
-import 'element-ui/lib/theme-chalk/index.css'
-
 /** 引入FilePreview */
 import FilePreview from '@/'
 Vue.use(FilePreview)
