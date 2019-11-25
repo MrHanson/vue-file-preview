3b66135bccaedc38c2f2f4ebb3ac31793ed052b1
MrHanson <chihanghanson@gmail.com>
2019-11-26 00:24:30 +0800

    refactor

--- App.vue
+++ App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <input type="file" @input="handleFileInput" />
+    <input type="file" multiple @input="handleFileInput" />
     <file-preview
       ref="filePV"
       :file-type="fileType"
