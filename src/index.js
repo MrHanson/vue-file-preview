3f090e2a436ec6353cacd17ecfd6921d8178df02
MrHanson <chihanghanson@gmail.com>
2019-12-10 14:52:13 +0800

    refactor

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 // all
 import FilePreview from './file-preview'
+
+FilePreview.install = function(Vue) {
+  Vue.component(FilePreview.name, FilePreview)
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  FilePreview.install(window.Vue)
+}
+
 export default FilePreview
 
 // previewers
 import ImgPreviewer from './previewers/img'
 import ExcelPrviewer from './previewers/excel'
-
-FilePreview.install = function(Vue) {
-  Vue.component(FilePreview.name, FilePreview)
-}
 
 ImgPreviewer.install = function(Vue) {
   Vue.component(ImgPreviewer.name, ImgPreviewer)
@@ -18,4 +23,4 @@ ExcelPrviewer.install = function(Vue) {
   Vue.component(ExcelPrviewer.name, ExcelPrviewer)
 }
 
-export const previewers = { ImgPreviewer, ExcelPrviewer }
+export { ImgPreviewer, ExcelPrviewer }
